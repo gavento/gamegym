e037f811df2c06f3aca4b43438777778126c589f
Tomas Gavenciak <gavento@ucw.cz>
2018-10-07 22:21:11 +0200

    Update strategy to use distributions.

--- gamegym/strategy.py
+++ gamegym/strategy.py
@@ -1,51 +1,28 @@
 #!/usr/bin/python3
 
 import random
-from .game import Game, GameState
+import game
+import distribution
 
 
 class Strategy:
-    def __init__(self, game, seed=None, rng=None):
-        assert isinstance(game, Game)
-        self.game = game
-        self.rng = rng or random.Random(seed)
+    def __init__(self, game_):
+        assert isinstance(game_, game.Game)
+        self.game = game_
 
-    def make_normalized_epsilon_greedy(self, actions, epsilon=0.0):
+    def distribution(self, state):
         """
-        Given a list/tuple of `Game.NextAction`, normalize their probalilities,
-        optionally making them epsilon-greedy.
-        """
-        assert isinstance(actions, (list, tuple))
-        assert isinstance(actions[0], Game.NextAction)
-        s = sum(a.probability for a in actions)
-        if s <= 1e-30: # In case all probs are 0
-            s = 1.0
-            epsilon = 1.0
-        return [Game.NextAction(
-            label, st, (1.0 - epsilon) * prob / s + epsilon / len(actions))
-            for label, st, prob in actions]
-
-    def distribution(self, state, epsilon=0.0):
-        """
-        Returns a list of `Game.NextAction` with the action probabilities.
-        Optionally make the distribution epsilon-greedy.
+        Returns a `Discrete` distribution on actions for the current state.
         """
         raise NotImplemented
 
-    def sample(self, state, *, epsilon=0.0, rng=None):
+
+class UniformStrategy(Strategy):
+    def distribution(self, state):
         """
-        Return one randomly chosen `Game.NextAction`, optionally using
-        provided rng and optionally chosing epsilon-greedily.
+        Returns a `Uniform` distribution on actions for the current state.
         """
-        dist = self.distribution(state, epsilon=epsilon)
-        p = (rng or self.rng).random()
-        s = 0.0
-        for na in dist:
-            s += na.probability
-            if p < s:
-                return na
-        assert s > 0.999
-        return na
+        return distribution.Uniform(state.actions())
 
 
 #class EpsilonProxy(Strategy):
