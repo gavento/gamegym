cd50201a31cc048d30f2c12a4d0f464f3c16c1e9
Tomas Gavenciak <gavento@ucw.cz>
2018-10-08 00:22:55 +0200

    Update WIP MCCFR to new API

--- gamegym/algorithms/mccfr.py
+++ gamegym/algorithms/mccfr.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 
-import random
 import collections
-from ..strategy import Strategy
+from .. import strategy, distribution
+from ..utils import get_rng
 
 
-class MCCFR(Strategy):
+class MCCFR(strategy.Strategy):
     Regret = collections.namedtuple("Regret", ("sum", "cnt"))
 
     def __init__(self, game, seed=None, rng=None):
-        super().__init__(game, seed=seed, rng=rng)
-        self.regret = {}  # (infoset, action_label) -> Regret
+        self.game = game
+        self.rng = get_rng(rng, seed)
+        self.regret = {}  # (player, infoset, action_label) -> Regret
 
-    def get_regret(self, infoset, action):
-        return self.regret.setdefault((infoset, action), self.Regret(0.0, 0))
+    def get_regret(self, player, infoset, action):
+        return self.regret.setdefault((player, infoset, action), self.Regret(0.0, 0))
 
-    def add_regret(self, infoset, action, r):
-        r0 = self.get_regret(infoset, action)
-        self.regret[(infoset, action)] = self.Regret(r0.sum + r)
+    def add_regret(self, player, infoset, action, r):
+        r0 = self.get_regret(player, infoset, action)
+        self.regret[(player, infoset, action)] = self.Regret(r0.sum + r, r0.cnt + 1)
 
-    def distribution(self, state, epsilon=0.0):
-        iset = state.information_set()
+    def distribution(self, state):
+        p = state.player()
+        infoset = state.information_set(p)
+        assert not (state.is_terminal() or state.is_chance())
         res = []
-        if state.is_terminal():
-            return []
-        for label, st, prob in state.actions():
-            if st.player() >= 0: # update for non-chance nodes
-                r = self.get_r(iset)
-                prob = max(r.sum / max(r.cnt, 1), 0.0)
-            res.append(self.game.NextAction(label, st, prob))
-        return self.make_normalized_epsilon_greedy(res, epsilon=epsilon)
+        for a in state.actions():
+            if state.player() >= 0: # update for non-chance nodes
+                reg = self.get_regret(p, infoset, a)
+                prob = max(reg.sum / max(reg.cnt, 1), 0.0)
+            res.append(prob)
+        return distribution.Explicit(res, state.actions(), normalize=True)
 
+    def compute(self, iterations, epsilon=0.1):
+        for _i in range(iterations):
+            play = self.generate_play(epsilon=epsion)
+            pass # TODO
 
-
-    def generate_play(self, state0, epsilon=0.0):
-        state = state0
-        play = [state0]
-        while not state.is_terminal():
-            self.get_strategy(state, epsilon=epsilon)
-
-            play.append(state)
-        return play
+    def generate_play(self, epsilon=0.0):
+        "Return simulated game state sequence (with exploration)."
+        prox = strategy.EpsilonUniformProxy(self, epsilon=epsilon)
+        return self.game.play_strategies(
+            [prox] * self.game.players(), rng=self.rng)
