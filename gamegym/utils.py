894581fc58db8962213eae4a375ab9696245822e
Tomas Gavenciak <gavento@ucw.cz>
2018-10-11 15:00:52 +0200

    Add progress bar report

--- gamegym/utils.py
+++ gamegym/utils.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 import numpy as np
+import contextlib
+import logging
+import time
 
 
 def get_rng(rng=None, seed=None):
@@ -13,3 +16,35 @@ def get_rng(rng=None, seed=None):
         rng = np.random.RandomState()
     assert isinstance(rng, np.random.RandomState)
     return rng
+
+
+class ProgressReporter:
+    def __init__(self, msg, limit, loglevel=logging.INFO, log=logging.root, exponent=1.5, min_time=5):
+        self.msg = msg
+        self.limit = limit
+        self.loglevel = loglevel
+        self.log = log
+        self.exponent = exponent
+        self.min_time = min_time
+
+    def __enter__(self):
+        self.start_time = time.clock()
+        self.last_time = time.clock()
+        self.last_val = 0
+        return self
+
+    def update(self, value):
+        t = time.clock()
+        dt = t - self.start_time
+        self.last_val = value
+        if dt > self.min_time and dt > self.exponent * (self.last_time - self.start_time):
+            eta = 1.0 * (self.limit - value) * dt / value
+            self.log.log(self.loglevel, "{}: {:5.2f}% ({} of {}), ETA +{:.2f}s".format(
+                self.msg, 100.0 * value / self.limit, value, self.limit, eta))
+            self.last_time = t
+
+    def __exit__(self, exception_type, exception_value, traceback):
+        t = time.clock()
+        dt = t - self.start_time
+        self.log.log(self.loglevel, "{} done in {:.2}s ({:.2f} / s)".format(
+            self.msg, dt, self.last_val / dt))
