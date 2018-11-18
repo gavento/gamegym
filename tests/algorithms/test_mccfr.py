6b6f1ec4815c8f19b4c1740cd46156f0a19d8208
Tomas Gavenciak <gavento@ucw.cz>
2018-11-18 18:07:59 +0100

    Update mccfr tests, add slow benchmark test

--- tests/algorithms/test_mccfr.py
+++ tests/algorithms/test_mccfr.py
@@ -3,22 +3,18 @@ import pytest
 import os
 
 from gamegym.games import MatchingPennies, RockPaperScissors, MatrixZeroSumGame, Goofspiel
-from gamegym.algorithms import BestResponse, OutcomeMCCFR
-
-
-def test_iterations():
-    pass
+from gamegym.algorithms import BestResponse, OutcomeMCCFR, exploitability
 
 
 def test_persist(tmpdir):
     g = MatchingPennies()
     mc = OutcomeMCCFR(g, seed=42)
-    fname = str(tmpdir.join("strat.pickle"))
+    fname = str(tmpdir.join("MatchingPennies"))
     assert mc.persist(fname, iterations=200) == False
     assert mc.iterations == 200
     
     mc2 = OutcomeMCCFR(g, seed=43)
-    assert mc2.persist(fname, iterations=60) == True
+    assert mc2.persist(fname, iterations=200) == True
     assert mc2.iterations == 200
    
 
@@ -42,7 +38,7 @@ def test_pennies():
     assert np.max(np.abs(mc.distribution(s).probabilities() - [0.5, 0.5])) < 0.1
 
 
-def test_mccfr_goofspiel():
+def test_mccfr_goofspiel3():
     g = Goofspiel(3)
     mc = OutcomeMCCFR(g, seed=56)
     mc.compute(500)
@@ -50,3 +46,11 @@ def test_mccfr_goofspiel():
     assert np.mean([
         g.play_strategies([br, mc], seed=i)[-1].values()[0]
         for i in range(200)]) == pytest.approx(0.0, abs=0.2)
+
+
+@pytest.mark.slow
+def test_mccfr_goofspiel4_slow():
+    g = Goofspiel(4, scoring=Goofspiel.Scoring.ZEROSUM)
+    mc = OutcomeMCCFR(g, seed=49)
+    mc.compute(10000)
+    assert exploitability(g, 0, mc) < 1.2
