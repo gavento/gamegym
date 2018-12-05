1742ce681023228616922dd0316f2d9fc665fb69
Tomas Gavenciak <gavento@ucw.cz>
2018-12-05 21:39:03 +0100

    Refactor best response, add limit, minor fix mccfr

--- gamegym/algorithms/bestresponse.py
+++ gamegym/algorithms/bestresponse.py
@@ -1,6 +1,5 @@
 from ..strategy import Strategy
-from ..distribution import Explicit
-from ..game import GameState, Game
+from ..game import GameState, Game, Active
 
 import collections
 import numpy as np
@@ -9,35 +8,42 @@ SupportItem = collections.namedtuple("SupportItem", ["state", "probability"])
 
 
 class BestResponse(Strategy):
-    def __init__(self, game, player, other_strategies):
+    def __init__(self, game, player, strategies, max_nodes=1e6):
+        assert isinstance(game, Game)
+        assert player < game.players
+        assert len(strategies) == game.players
+        nodes = 0
 
         # DFS for from state to terminal state or stata of "player"
         def trace(state, probability, supports):
+            nonlocal nodes
+            nodes += 1
+            if nodes > max_nodes:
+                raise Exception(
+                    "BestResponse traversed more than allowed {} nodes.".format(max_nodes) +
+                    "Either increase the limit or consider using approximate best response.")
             # Just to get rid of nodes where distrbution returned pure zero
             if probability == 0.0:
                 return 0.0
-            p = state.player()
+            p = state.active.player
             if p == player:
-                pi = state.player_information(player)
-                s = supports.get(pi)
-                if s is None:
-                    s = []
-                    supports[pi] = s
+                pi = state.observations[player]
+                s = supports.setdefault(pi, list())
                 s.append(SupportItem(state, probability))
                 return 0
-            if p == GameState.P_TERMINAL:
-                return state.values()[player] * probability
-            if p == GameState.P_CHANCE:
-                distribution = state.chance_distribution()
+            if p == Active.TERMINAL:
+                return state.active.payoff[player] * probability
+            if p == Active.CHANCE:
+                distribution = state.active.chance
             else:
-                distribution = other_strategies[p].distribution(state)
+                distribution = strategies[p].distribution(state.observations[p], state.active)
             return sum(
-                trace(state.play(action), pr * probability, supports)
-                for pr, action in zip(distribution.probabilities(), distribution.values()))
+                trace(game.play(state, action), pr * probability, supports)
+                for pr, action in zip(distribution, state.active.actions))
 
         # DFS from isets to other isets of "player"
         def traverse(iset, support):
-            actions = support[0].state.actions()
+            actions = support[0].state.active.actions
             values = []
             br_list = []
             for action in actions:
@@ -47,7 +53,7 @@ class BestResponse(Strategy):
                 br_list.append(best_responses)
 
                 for s in support:
-                    value += trace(s.state.play(action), s.probability, new_supports)
+                    value += trace(game.play(s.state, action), s.probability, new_supports)
                 for iset2, s in new_supports.items():
                     v, br = traverse(iset2, s)
                     value += v
@@ -59,7 +65,8 @@ class BestResponse(Strategy):
             mx = values.max()
             is_best = values >= (mx - mx * 0e-6)
             br_result = {}
-            br_result[iset] = Explicit(is_best.astype(np.float), actions, normalize=True)
+            bdist = is_best.astype(np.float)
+            br_result[iset] = bdist / sum(bdist)
             for br, is_b in zip(br_list, is_best):
                 if is_b:
                     br_result.update(br)
@@ -67,23 +74,24 @@ class BestResponse(Strategy):
 
         supports = {}
         self.best_responses = {}
-        value = trace(game.initial_state(), 1.0, supports)
+        value = trace(game.start(), 1.0, supports)
         for iset2, s in supports.items():
             v, br = traverse(iset2, s)
             value += v
             self.best_responses.update(br)
         self.value = value
 
-    def distribution(self, state):
-        return self.best_responses[state.player_information(state.player())]
+    def distribution(self, observation, _active):
+        return self.best_responses[observation]
 
 
-def exploitability(game, measured_player, strategy):
+def exploitability(game, measured_player, strategy, max_nodes=1e6):
     """
     Exact exploitability of a player strategy in a two player ZERO-SUM game.
     """
     assert measured_player in (0, 1)
     assert isinstance(game, Game)
+    assert game.players == 2
     assert isinstance(strategy, Strategy)
-    BR = BestResponse(game, 1 - measured_player, [strategy, strategy])
-    return BR.value
+    br = BestResponse(game, 1 - measured_player, [strategy, strategy], max_nodes)
+    return br.value
--- tests/algorithms/test_bestresponse.py
+++ tests/algorithms/test_bestresponse.py
@@ -1,24 +1,19 @@
 from gamegym.games import RockPaperScissors, Goofspiel
-from gamegym.strategy import FixedStrategy, UniformStrategy
+from gamegym.strategy import ConstStrategy, UniformStrategy
 from gamegym.algorithms import BestResponse
-from gamegym.distribution import Explicit
 import pytest
 
 
 def test_best_response_rps():
 
-    bart_simpson_strategy = FixedStrategy(Explicit([1, 0, 0], values=["R", "P", "S"]))
+    bart_simpson_strategy = ConstStrategy((1, 0, 0))
     game = RockPaperScissors()
-    strategy = BestResponse(game, 0, {1: bart_simpson_strategy})
-    assert list(strategy.best_responses.values())[0].probability("R") == 0.0
-    assert list(strategy.best_responses.values())[0].probability("P") == 1.0
-    assert list(strategy.best_responses.values())[0].probability("S") == 0.0
+    strategy = BestResponse(game, 0, [bart_simpson_strategy] * 2)
+    assert tuple(strategy.best_responses.values())[0] == pytest.approx((0.0, 1.0, 0.0))
     assert strategy.value == pytest.approx(1.0)
 
-    strategy = BestResponse(game, 1, {0: bart_simpson_strategy})
-    assert list(strategy.best_responses.values())[0].probability("R") == 0.0
-    assert list(strategy.best_responses.values())[0].probability("P") == 1.0
-    assert list(strategy.best_responses.values())[0].probability("S") == 0.0
+    strategy = BestResponse(game, 1, [bart_simpson_strategy] * 2)
+    assert tuple(strategy.best_responses.values())[0] == pytest.approx((0.0, 1.0, 0.0))
     assert strategy.value == pytest.approx(1.0)
 
 
@@ -27,8 +22,10 @@ def test_best_response_goofspiel():
 
     for n_cards, br_value in [(3, pytest.approx(4 / 3)), (4, pytest.approx(2.5))]:
         game = Goofspiel(n_cards, Goofspiel.Scoring.ZEROSUM)
-        strategy = BestResponse(game, 0, {1: UniformStrategy()})
+        strategy = BestResponse(game, 0, [UniformStrategy()] * 2)
         for k, v in strategy.best_responses.items():
-            reward = k[1][-1]
-            assert reward not in v.values() or v.probability(reward) == 1.0
+            reward = k[-1].obs
+            played_cards = [o.obs for o in k[0::2]]
+            idx = len([i for i in range(n_cards) if i < reward and i not in played_cards])
+            assert reward in played_cards or v[idx] == 1.0
         assert strategy.value == br_value
