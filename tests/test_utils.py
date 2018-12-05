20a8cca75305463aefe27c7f68ffe756f6230b58
Tomas Gavenciak <gavento@ucw.cz>
2018-12-06 00:07:07 +0100

    Add simple distributions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from gamegym.utils import debug_assert, get_rng, uniform, np_uniform
+from gamegym.utils import debug_assert, get_rng, uniform, np_uniform, sample_with_p, Distribution
 import numpy as np
 import pytest
 import random
@@ -27,3 +27,25 @@ def test_uniform():
     assert (np_uniform(2) == np.array([0.5, 0.5])).all()
     assert len(np_uniform(10)) == 10
     assert np.sum(np_uniform(10)) == pytest.approx(1.0)
+
+
+def test_sample_with_p():
+    for i in range(50):
+        assert sample_with_p("abcd", None)[0] in "abcd"
+        assert sample_with_p("abcd", None)[1] == 0.25
+        assert sample_with_p(8, None)[1] == 0.125
+        assert sample_with_p(1, None)[1] == 1.0
+        assert sample_with_p(None, [0.1, 0.8, 0.1])[0] in (0, 1, 2)
+        assert sample_with_p(None, [0.1, 0.8, 0.1])[1] in (0.1, 0.8)
+        assert sample_with_p(["a", "b"], [0.2, 0.8]) in (("a", 0.2), ("b", 0.8))
+
+
+def test_distribution():
+    for i in range(50):
+        assert Distribution("abcd", None).sample() in "abcd"
+        assert Distribution("abcd", None).sample_with_p()[1] == 0.25
+        assert Distribution(8, None).sample_with_p()[1] == 0.125
+        assert Distribution(1, None).sample_with_p()[1] == 1.0
+        assert Distribution(None, [0.1, 0.8, 0.1]).sample_with_p()[0] in (0, 1, 2)
+        assert Distribution(None, [0.1, 0.8, 0.1]).sample_with_p()[1] in (0.1, 0.8)
+        assert Distribution(["a", "b"], [0.2, 0.8]).sample_with_p() in (("a", 0.2), ("b", 0.8))
