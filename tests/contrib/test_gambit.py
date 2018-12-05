0e74b0df5ab4a978c6afe9ce2237b5ba9ce5b094
Tomas Gavenciak <gavento@ucw.cz>
2018-12-05 20:51:45 +0100

    Fix gambit and move to contrib, move out features

---
+++ tests/contrib/test_gambit.py
@@ -0,0 +1,21 @@
+import pytest
+import io
+from gamegym.games import Goofspiel
+from gamegym.contrib.gambit import write_efg
+
+
+def test_dump_gambit():
+    g = Goofspiel(3, scoring=Goofspiel.Scoring.ZEROSUM)
+
+    s = io.StringIO()
+    write_efg(g, s, names=False)
+    assert (len(s.getvalue()) > 1024)
+
+    s = io.StringIO()
+    write_efg(g, s, names=True)
+    assert (len(s.getvalue()) > 1024)
+
+    g2 = Goofspiel(2, scoring=Goofspiel.Scoring.WINLOSS)
+    s = io.StringIO()
+    write_efg(g2, s, names=True)
+    assert len(s.getvalue().splitlines()) == 40
