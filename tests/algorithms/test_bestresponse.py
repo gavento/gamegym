6f9ff89d4ba809c857154e66342e4aec3d64a45e
Tomas Gavenciak <gavento@ucw.cz>
2018-12-06 02:53:30 +0100

    Add approx best response and exploitability, minor polish

--- tests/algorithms/test_bestresponse.py
+++ tests/algorithms/test_bestresponse.py
@@ -1,19 +1,26 @@
 from gamegym.games import RockPaperScissors, Goofspiel
 from gamegym.strategy import ConstStrategy, UniformStrategy
-from gamegym.algorithms import BestResponse
+from gamegym.algorithms import BestResponse, ApproxBestResponse
 import pytest
 
 
 def test_best_response_rps():
     bart_simpson_strategy = ConstStrategy((1, 0, 0))
     game = RockPaperScissors()
-    strategy = BestResponse(game, 0, [bart_simpson_strategy] * 2)
-    assert tuple(strategy.best_responses.values())[0] == pytest.approx((0.0, 1.0, 0.0))
-    assert strategy.value == pytest.approx(1.0)
+    for p in [0, 1]:
+        strategy = BestResponse(game, p, [bart_simpson_strategy] * 2)
+        assert tuple(strategy.best_responses.values())[0] == pytest.approx((0.0, 1.0, 0.0))
+        assert strategy.value == pytest.approx(1.0)
 
-    strategy = BestResponse(game, 1, [bart_simpson_strategy] * 2)
-    assert tuple(strategy.best_responses.values())[0] == pytest.approx((0.0, 1.0, 0.0))
-    assert strategy.value == pytest.approx(1.0)
+
+def test_approx_best_response_rps():
+    bart_simpson_strategy = ConstStrategy((1, 0, 0))
+    game = RockPaperScissors()
+
+    for p in [0, 1]:
+        s = ApproxBestResponse(game, 0, [bart_simpson_strategy] * 2, iterations=200, seed=23)
+        assert s.distribution((), 3) == pytest.approx((0.0, 1.0, 0.0))
+        assert s.sample_value(50) == pytest.approx(1.0)
 
 
 @pytest.mark.slow
@@ -27,6 +34,19 @@ def test_best_response_goofspiel():
             idx = len([i for i in range(n_cards) if i < reward and i not in played_cards])
             assert reward in played_cards or v[idx] == 1.0
         assert strategy.value == br_value
+
+
+@pytest.mark.slow
+def test_approx_best_response_goofspiel():
+    for n_cards, its, br_value in [(3, 1000, 1.333), (4, 10000, 2.5)]:
+        game = Goofspiel(n_cards, Goofspiel.Scoring.ZEROSUM)
+        strategy = ApproxBestResponse(game, 0, [UniformStrategy()] * 2, iterations=its, seed=35)
+        #for k, v in strategy.best_responses.items():
+        #    reward = k[-1].obs
+        #    played_cards = [o.obs for o in k[0::2]]
+        #    idx = len([i for i in range(n_cards) if i < reward and i not in played_cards])
+        #    assert reward in played_cards or v[idx] == 1.0
+        assert strategy.sample_value(its // 2) == pytest.approx(br_value, rel=0.2)
 
 
 def test_best_response_limit():
