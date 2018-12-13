fb83e558a9b895dac6f35f85a102d1f84e293204
Tomas Gavenciak <gavento@ucw.cz>
2018-12-13 18:37:31 +0100

    Add gambit strategy parsing (partial)

--- tests/contrib/test_gambit.py
+++ tests/contrib/test_gambit.py
@@ -1,10 +1,11 @@
 import pytest
 import io
 from gamegym.games import Goofspiel
-from gamegym.contrib.gambit import write_efg
+from gamegym.contrib.gambit import write_efg, parse_strategy
+from gamegym.algorithms import exploitability
 
 
-def test_dump_gambit():
+def test_dump_gambit_game():
     g = Goofspiel(3, scoring=Goofspiel.Scoring.ZEROSUM)
 
     s = io.StringIO()
@@ -19,3 +20,20 @@ def test_dump_gambit():
     s = io.StringIO()
     write_efg(g2, s, names=True)
     assert len(s.getvalue().splitlines()) == 40
+
+
+def test_parse_gambit_strategy_g2():
+    g = Goofspiel(2, scoring=Goofspiel.Scoring.ZEROSUM)
+    txt = "NE,1,0,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1"
+    strats = parse_strategy(g, txt)
+    assert exploitability(g, 0, strats[0]) < 1e-6
+    assert exploitability(g, 0, strats[1]) < 1e-6
+
+
+@pytest.mark.xfail(reason="Waiting for analsis of verbose output from `gambit-lcp -D g3.efg`")
+def test_parse_gambit_strategy_g3():
+    g = Goofspiel(3, scoring=Goofspiel.Scoring.ZEROSUM)
+    txt = "NE,1,0,0,1,0,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,1,0,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,0,0,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,0,0,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,0,0,1,0,1,1,1,1,0,1,1,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,0,0,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,0,0,1,1,1,1,0,0,1,1,1,1,1,0,1,1,1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,0,0,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,0,0,1,1,1,0,1,1,1,1,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,0,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1"
+    strats = parse_strategy(g, txt)
+    assert exploitability(g, 0, strats[0]) < 1e-6
+    assert exploitability(g, 0, strats[1]) < 1e-6
