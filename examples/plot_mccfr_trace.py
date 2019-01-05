52c163d239a7957e1e01cfca34476c95720051d7
Tomas Gavenciak <gavento@ucw.cz>
2019-01-05 20:21:14 +0100

    Update plot_mccfr_trace: use plotly, PCA base with weights

--- examples/plot_mccfr_trace.py
+++ examples/plot_mccfr_trace.py
@@ -1,68 +1,68 @@
-from gamegym.algorithms import OutcomeMCCFR
-from gamegym.games import Goofspiel, MatchingPennies, DicePoker
-from gamegym.contrib.plot_strategy import PlotStrategyPCA
-
-import bokeh.plotting, bokeh.palettes
 import logging
+
+import bokeh.palettes
+import bokeh.plotting
+import numpy as np
+import plotly
+import plotly.graph_objs as pgo
 import tqdm
+
+from gamegym.algorithms import OutcomeMCCFR
+from gamegym.contrib.plot_strategy import PlotStrategyPCA
+from gamegym.games import DicePoker, Goofspiel, MatchingPennies
+
 #logging.basicConfig(level=logging.INFO)
 
 
-def plot_to_files(g, prefix, n_traces, iters, steps, depth=6, base=None, burn=None):
+def plot_to_files(g, prefix, n_traces, iters, steps, depth=6, base=None, burn=None, burn_from=0):
     pal = bokeh.palettes.Dark2_8
-    bokeh.plotting.output_file("/tmp/bokeh_dummy", mode="cdn")
-
+    
     traces = []
-    for ti in tqdm.trange(n_traces, desc=prefix): 
+    for ti in tqdm.trange(n_traces, desc=prefix):
+        name="MCCFR run #{}".format(ti)
+        if burn and ti >= burn_from:
+            name += " (burn-in)"
         mc = OutcomeMCCFR(g, seed=hash(str(g)) % 2**30 + ti)
-        ps = PlotStrategyPCA(g, depth=depth, name="MCCFR {}".format(ti))
+        ps = PlotStrategyPCA(g, depth=depth, name=name)
         for i in tqdm.trange(steps, desc="MCCFR steps"):
             w = 1.0
-            if burn and i < steps * burn:
+            if burn and ti >= burn_from and i < steps * burn:
                 w = 0.03**(1.0 - float(i) / steps / burn)
-            mc.compute(iters // steps, progress=False, weight=w)
+            mc.compute(int(iters * (i + 1) / steps) - mc.iterations, progress=False, weight=w)
             ps.append(mc.iterations, mc.strategies)
         traces.append(ps)
 
-    if base is None:
-        base = PlotStrategyPCA.common_base(traces, with_regret=True)
 
-    title = "Strategy trajectories of {} in {} iters".format(g, iters)
+    title = "Strategy trajectories of {} in {} iters, {} steps".format(g, iters, steps)
     if n_traces > 1:
-        fig = bokeh.plotting.figure(title=title)
-        PlotStrategyPCA.common_plot(fig, traces, base=base, with_regret=False, palette=pal)
-        bokeh.plotting.save(fig, prefix + "_all.html", title=title)
-
-        fig = bokeh.plotting.figure(title=title)
-        PlotStrategyPCA.common_plot(fig, traces, base=base, with_regret=True, palette=pal)
-        bokeh.plotting.save(fig, prefix + "_all_with_regrets.html", title=title)
+        if base is None:
+            w_vecs = [t.infoset_weight_vec(mc.strategies) for t in traces]
+            base = PlotStrategyPCA.common_base(traces, with_regret=True, weight_vec=np.mean(w_vecs, axis=0))
+        objs = PlotStrategyPCA.common_plot(traces, base=base, with_regret=True, palette=pal)
+        fig = pgo.Figure(objs, layout=pgo.Layout(hovermode='closest', title=dict(text=title)))
+        plotly.offline.plot(fig, filename=prefix + "_all.html", auto_open=False, include_plotlyjs='cdn')
 
     for ti in range(n_traces):
-        fig = bokeh.plotting.figure(title=title)
-        traces[ti].plot(fig, base=base, color=pal[ti], with_regret=True)
-        bokeh.plotting.save(fig, prefix + "_{}.html".format(ti), title=title)
-
-        fig = bokeh.plotting.figure(title=title)
-        traces[ti].plot(fig, color=pal[ti], with_regret=True)
-        bokeh.plotting.save(fig, prefix + "_{}_ownPCA.html".format(ti), title=title)
+        objs = traces[ti].plot(color=pal[ti], with_regret=True)
+        fig = pgo.Figure(objs, layout=pgo.Layout(hovermode='closest', title=dict(text=title + "(independent PCA)")))
+        plotly.offline.plot(fig, filename=prefix + "_{}.html".format(ti), auto_open=False, include_plotlyjs='cdn')
 
     return base
 
 
 def main():
     g = MatchingPennies()
-    plot_to_files(g, "plot_mccfr_trace_pennies", 3, 1000, 100)
+    base = np.array([[1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0]])
+    plot_to_files(g, "plot_mccfr_trace_pennies", 3, 500, 100, base=base)
 
     g = Goofspiel(4, scoring=Goofspiel.Scoring.ZEROSUM)
-    base = plot_to_files(g, "plot_mccfr_trace_goof4", 3, 50000, 100, depth=6)
-    plot_to_files(g, "plot_mccfr_trace_goof4burn", 3, 50000, 100, depth=6, base=base, burn=0.3)
+    #plot_to_files(g, "plot_mccfr_trace_goof4", 6, 50000, 200, depth=6, burn=0.3, burn_from=3)
 
-    return
     g = DicePoker()
-    plot_to_files(g, "plot_mccfr_trace_dicepoker", 3, 50000, 100, depth=10)
+    plot_to_files(g, "plot_mccfr_trace_dicepoker", 6, 200000, 200, depth=8, burn=0.3, burn_from=3)
 
     g = Goofspiel(5, scoring=Goofspiel.Scoring.ZEROSUM)
-    plot_to_files(g, "plot_mccfr_trace_goof5", 3, 100000, 100, depth=6)
+    #plot_to_files(g, "plot_mccfr_trace_goof5", 3, 100000, 200, depth=6)
 
 
 if __name__ == '__main__':
--- gamegym/contrib/plot_strategy.py
+++ gamegym/contrib/plot_strategy.py
@@ -8,79 +8,128 @@ class PlotStrategyPCA:
         self.depth = depth
         self.name = name
 
+        infosets = set()
+        def _traverse(state, d):
+            if state.is_terminal() or d >= self.depth:
+                return
+            if not state.is_chance():
+                infosets.add((state.player, state.observations[state.player], state.actions))
+            for ai, a in enumerate(state.actions):
+                _traverse(self.game.play(state, index=ai), d + 1)
+        _traverse(self.game.start(), 0)
+        self.infosets = sorted(list(infosets))
+
         self.d_t = []
         self.d_strat = []
         self.d_regret = []
 
-    def _traverse(self, strategies, state, d, p_strat, p_reg, strat_vec, reg_vec):
-        if d >= self.depth:
-            return
-        if state.is_terminal():
-            return
-        if state.is_chance():
-            strat_pol = state.chance
-            reg_pol = strat_pol
-        else:
-            s = strategies[state.player]
-            strat_pol = s.strategy(state)
-            if isinstance(s, RegretStrategy):
-                obs = state.observations[state.player]
-                entry = s.get_entry(obs, len(state.actions))
-                reg_pol = s.regret_matching(entry[0])                
-            else:
-                reg_pol = strat_pol
-            strat_vec.extend(p_strat * np.array(strat_pol))
-            reg_vec.extend(p_reg * np.array(reg_pol))
-
-        for ai, _ in enumerate(state.actions):
-            state2 = self.game.play(state, index=ai)
-            self._traverse(strategies, state2, d + 1, p_strat * strat_pol[ai], p_reg * reg_pol[ai], strat_vec, reg_vec)
-        return (strat_vec, reg_vec)
-
     def append(self, t, strategies):
         assert len(strategies) == self.game.players
         assert (not self.d_t) or self.d_t[-1] < t
-        strat_vec, reg_vec = self._traverse(strategies, self.game.start(), 0, 1.0, 1.0, [], [])
+
+        strat_vec = []
+        reg_vec = []
+        for si, iset, actions in self.infosets:
+            s = strategies[si]
+            strat = s.strategy(iset, len(actions))
+            reg = strat
+            if isinstance(s, RegretStrategy):
+                entry = s.get_entry(iset, len(actions))
+                reg = s.regret_matching(entry[0])
+            strat_vec.extend(strat)
+            reg_vec.extend(reg)
+
         self.d_t.append(t)
         self.d_strat.append(np.array(strat_vec))
         self.d_regret.append(np.array(reg_vec))
 
-    def plot(self, fig, base=None, color=None, smooth_regret=11, with_regret=False):
+    def infoset_weight_vec(self, strategies):
+        assert len(strategies) == self.game.players
+        strat_ps = {i: 0.0 for i in self.infosets}
+        #reg_ps = {i: 0.0 for i in self.infosets}
+
+        def _traverse(state, strat_p, reg_p, d):
+            iset = state.observations[state.player]
+            idx = (state.player, iset, state.actions)
+            if state.is_terminal() or d >= self.depth:
+                return
+            if not state.is_chance():
+                strat_ps[idx] += strat_p
+                #reg_ps[idx] += reg_p
+                s = strategies[state.player]
+                strat = s.strategy(state)
+                reg = strat
+                if isinstance(s, RegretStrategy):
+                    entry = s.get_entry(iset, len(state.actions))
+                    reg = s.regret_matching(entry[0])
+            else:
+                strat = state.chance
+                reg = strat
+            for ai, a in enumerate(state.actions):
+                _traverse(self.game.play(state, index=ai), strat_p * strat[ai], reg_p * reg[ai], d + 1)
+
+        _traverse(self.game.start(), 1.0, 1.0, 0)
+        strat_vec = []
+        #reg_vec = []
+        for idx in self.infosets:
+            strat_vec.extend([strat_ps[idx]] * len(idx[2]))
+            #reg_vec.extend([reg_ps[idx]] * len(idx[2]))
+        return np.array(strat_vec)  #, np.array(reg_vec))
+
+    def plot(self, base=None, color=None, smooth_regret=0, with_regret=False):
+        import plotly.graph_objs as pgo
+
         d_t = np.array(self.d_t)
+        d_text = ["{} its".format(t) for t in d_t]
         d_strat = np.array(self.d_strat)
         d_regret = np.array(self.d_regret)
         if base is None:
             base = self.common_base([self], with_regret=with_regret)
         assert base.shape == (2, d_strat.shape[1])
+        
         proj_strat = np.matmul(d_strat, base.transpose())
-        fig.line(proj_strat[:, 0], proj_strat[:, 1], legend=self.name + " strategy", color=color, line_width=1.8)
-        fig.x(proj_strat[-1, 0], proj_strat[-1, 1], color=color, line_width=3.0, size=10)
+        objs = [
+            pgo.Scatter(x=proj_strat[:, 0], y=proj_strat[:, 1], name=self.name + " strategy", 
+                        mode="lines+markers", hovertext=d_text, hoverinfo="text+name", 
+                        line=dict(color=color, width=1.8, shape='spline'),
+                        marker=dict(symbol="x", color=color, size=[0] * (proj_strat.shape[0] - 1) + [10])),
+        ]
         if with_regret:
             proj_regret = np.matmul(d_regret, base.transpose())
             if smooth_regret > 1:
                 from scipy import signal
                 proj_regret = signal.savgol_filter(proj_regret, smooth_regret, min(4, smooth_regret - 1), axis=0)
-            fig.line(proj_regret[:, 0], proj_regret[:, 1], legend=self.name + " regret", alpha=0.7, color=color)
-            fig.x(proj_regret[-1, 0], proj_regret[-1, 1], fill_color=None, color=color, size=10, line_width=1.8)
-        return (base, fig)
+            objs.extend([
+                pgo.Scatter(x=proj_regret[:, 0], y=proj_regret[:, 1], name=self.name + " regret", 
+                            mode="lines+markers", hovertext=d_text, hoverinfo="text+name", 
+                            line=dict(color=color, width=0.7, shape='spline', smoothing=0.3),
+                            marker=dict(symbol="x", color=color, opacity=0.7, size=[0] * (proj_regret.shape[0] - 1) + [10])),
+            ])
+        return objs
 
     @classmethod
-    def common_base(cls, traces, with_regret=False):
+    def common_base(cls, traces, with_regret=False, weight_vec=None):
         import sklearn
         from sklearn.decomposition import PCA
+
         rows = [t.d_strat for t in traces]
         if with_regret:
             rows.extend([t.d_regret for t in traces])
         pca = PCA(2)
-        pca.fit(np.concatenate(rows, axis=0))
+        rows = np.concatenate(rows, axis=0)
+        if weight_vec is not None:
+            rows *= [weight_vec] 
+        pca.fit(rows)
         return pca.components_
 
     @classmethod
-    def common_plot(cls, fig, traces, base=None, palette=None, with_regret=False):
+    def common_plot(cls, traces, base=None, palette=None, with_regret=False):
         if base is None:
             base = cls.common_base(traces, with_regret=with_regret)
         if palette is None:
             import bokeh.palettes
             palette = bokeh.palettes.Category20_20
+        objs = []
         for ti, t in enumerate(traces):
-            t.plot(fig, base=base, color=palette[ti], with_regret=with_regret)
+            objs.extend(t.plot(base=base, color=palette[ti], with_regret=with_regret))
+        return objs
