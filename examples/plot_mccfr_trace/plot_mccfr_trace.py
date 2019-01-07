dde0852373522b580a50f6128e1d0964fc8344dc
Tomas Gavenciak <gavento@ucw.cz>
2019-01-07 01:59:36 +0100

    Update trace plotting (add exploit) and examples

--- examples/plot_mccfr_trace/plot_mccfr_trace.py
+++ examples/plot_mccfr_trace/plot_mccfr_trace.py
@@ -1,51 +1,131 @@
 import logging
 
-import bokeh.palettes
-import bokeh.plotting
 import numpy as np
 import plotly
 import plotly.graph_objs as pgo
 import tqdm
 
-from gamegym.algorithms import OutcomeMCCFR
-from gamegym.contrib.plot_strategy import PlotStrategyPCA
+from gamegym.algorithms import OutcomeMCCFR, exploitability, BestResponse
+from gamegym.contrib.plot_strategy import StrategyTrace
 from gamegym.games import DicePoker, Goofspiel, MatchingPennies
+from gamegym.utils import cached
+from gamegym.strategy import UniformStrategy
 
-#logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
+
+# How to include PlotlyJS in output: 'cdn', 'directory', True
+INCL_JS = 'cdn'
 
 
-def plot_to_files(g, prefix, n_traces, iters, steps, depth=6, base=None, burn=None, burn_from=0):
-    pal = bokeh.palettes.Dark2_8
-    
+@cached
+def compute_mccfr_traces(g,
+                         prefix,
+                         n_traces,
+                         iters,
+                         steps,
+                         depth=6,
+                         burn=None,
+                         burn_from=0,
+                         add_uniform=True,
+                         exploit_every=None,
+                         eploit_max_nodes=1e6):
+    """
+    Computes independent strategy traces of MCCFR in game `g`.
+    """
     traces = []
     for ti in tqdm.trange(n_traces, desc=prefix):
-        name="MCCFR run #{}".format(ti)
+        name = "MCCFR run #{}".format(ti)
         if burn and ti >= burn_from:
             name += " (burn-in)"
         mc = OutcomeMCCFR(g, seed=hash(str(g)) % 2**30 + ti)
-        ps = PlotStrategyPCA(g, depth=depth, name=name)
+        ps = StrategyTrace(g, depth=depth, name=name)
         for i in tqdm.trange(steps, desc="MCCFR steps"):
             w = 1.0
             if burn and ti >= burn_from and i < steps * burn:
                 w = 0.03**(1.0 - float(i) / steps / burn)
             mc.compute(int(iters * (i + 1) / steps) - mc.iterations, progress=False, weight=w)
-            ps.append(mc.iterations, mc.strategies)
+            exps = None
+            if exploit_every is not None and (steps - i - 1) % exploit_every == 0:
+                exps = [
+                    exploitability(g, p, mc.strategies[p], max_nodes=eploit_max_nodes)
+                    for p in range(g.players)
+                ]
+            ps.append(mc.iterations, mc.strategies, exps)
         traces.append(ps)
 
+    if add_uniform:
+        rps = StrategyTrace(g, depth=depth, name="Uniform")
+        rstrat = [UniformStrategy()] * g.players
+        rexps = None
+        if exploit_every is not None:
+            rexps = [
+                exploitability(g, p, rstrat[p], max_nodes=eploit_max_nodes)
+                for p in range(g.players)
+            ]
+        for t in traces[0].d_t:
+            rps.append(t, rstrat, rexps)
+    traces.append(rps)
 
+    return traces
+
+
+def plot_to_files(g,
+                  prefix,
+                  n_traces,
+                  iters,
+                  steps,
+                  depth=6,
+                  random_projs=3,
+                  base=None,
+                  burn=None,
+                  exploit_every=None,
+                  burn_from=0):
+    logging.info("Plotting " + prefix)
+    traces = compute_mccfr_traces(
+        g,
+        prefix,
+        n_traces,
+        iters,
+        steps,
+        depth=depth,
+        burn=burn,
+        burn_from=burn_from,
+        exploit_every=exploit_every,
+        eploit_max_nodes=1e8)
     title = "Strategy trajectories of {} in {} iters, {} steps".format(g, iters, steps)
+    pal = plotly.colors.DEFAULT_PLOTLY_COLORS
+    w_vec = np.mean([t.infoset_weight_vec(t.last_strategies) for t in traces], axis=0)
+
+    # Common PCA plot
     if n_traces > 1:
         if base is None:
-            w_vecs = [t.infoset_weight_vec(mc.strategies) for t in traces]
-            base = PlotStrategyPCA.common_base(traces, with_regret=True, weight_vec=np.mean(w_vecs, axis=0))
-        objs = PlotStrategyPCA.common_plot(traces, base=base, with_regret=True, palette=pal)
-        fig = pgo.Figure(objs, layout=pgo.Layout(hovermode='closest', title=dict(text=title)))
-        plotly.offline.plot(fig, filename=prefix + "_all.html", auto_open=False, include_plotlyjs='cdn')
+            base = StrategyTrace.common_PCA_base(traces, weight_vec=w_vec)
+        fig = StrategyTrace.common_plot(traces, base=base, palette=pal, title=title)
+        plotly.offline.plot(
+            fig, filename=prefix + "_PCA_all.html", auto_open=False, include_plotlyjs=INCL_JS)
 
-    for ti in range(n_traces):
-        objs = traces[ti].plot(color=pal[ti], with_regret=True)
-        fig = pgo.Figure(objs, layout=pgo.Layout(hovermode='closest', title=dict(text=title + "(independent PCA)")))
-        plotly.offline.plot(fig, filename=prefix + "_{}.html".format(ti), auto_open=False, include_plotlyjs='cdn')
+    # Individual PCA plots
+    for ti, t in enumerate(traces):
+        if t.name.startswith('Uniform'):
+            continue
+        t_w_vec = t.infoset_weight_vec(t.last_strategies)
+        t_base = StrategyTrace.common_PCA_base([t], weight_vec=t_w_vec, seed=42)
+        fig = StrategyTrace.common_plot([t], base=t_base, palette=[pal[ti]])
+        plotly.offline.plot(
+            fig,
+            filename=prefix + "_PCA_{}.html".format(ti),
+            auto_open=False,
+            include_plotlyjs=INCL_JS)
+
+    # Common plot with random projections
+    for rpi in range(random_projs):
+        r_base = StrategyTrace.common_random_base(traces, weight_vec=w_vec, seed=43)
+        fig = StrategyTrace.common_plot(traces, base=r_base, palette=pal)
+        plotly.offline.plot(
+            fig,
+            filename=prefix + "_Rand{}_all.html".format(rpi),
+            auto_open=False,
+            include_plotlyjs=INCL_JS)
 
     return base
 
@@ -53,16 +133,16 @@ def plot_to_files(g, prefix, n_traces, iters, steps, depth=6, base=None, burn=No
 def main():
     g = MatchingPennies()
     base = np.array([[1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0]])
-    plot_to_files(g, "plot_mccfr_trace_pennies", 3, 500, 100, base=base)
+    plot_to_files(g, "plot_mccfr_trace_pennies", 3, 500, 100, base=base, exploit_every=1)
+
+    g = DicePoker(6)
+    plot_to_files(g, "plot_mccfr_trace_dicepoker", 6, 100000, 200, depth=6, burn=0.3, burn_from=3, exploit_every=1)
 
     g = Goofspiel(4, scoring=Goofspiel.Scoring.ZEROSUM)
-    #plot_to_files(g, "plot_mccfr_trace_goof4", 6, 50000, 200, depth=6, burn=0.3, burn_from=3)
-
-    g = DicePoker()
-    plot_to_files(g, "plot_mccfr_trace_dicepoker", 6, 200000, 200, depth=8, burn=0.3, burn_from=3)
+    plot_to_files(g, "plot_mccfr_trace_goof4", 6, 100000, 200, depth=6, burn=0.3, burn_from=3, exploit_every=1)
 
     g = Goofspiel(5, scoring=Goofspiel.Scoring.ZEROSUM)
-    #plot_to_files(g, "plot_mccfr_trace_goof5", 3, 100000, 200, depth=6)
+    plot_to_files(g, "plot_mccfr_trace_goof5", 6, 200000, 200, depth=6, burn=0.3, burn_from=3, exploit_every=10)
 
 
 if __name__ == '__main__':
--- gamegym/contrib/plot_strategy.py
+++ gamegym/contrib/plot_strategy.py
@@ -1,14 +1,26 @@
 import numpy as np
+import scipy as sp
 
 from ..algorithms.mccfr import RegretStrategy
 
-class PlotStrategyPCA:
-    def __init__(self, game, depth=4, with_regret=None, name=""):
+
+class StrategyTrace:
+    """
+    Record the progress of strategy computation.
+
+    Has several visualisation and projection methods.
+    Tracks the behavior in all information sets encountered up to given depth.
+    """
+
+    def __init__(self, game, depth, name=""):
         self.game = game
         self.depth = depth
         self.name = name
+        # last added strategies
+        self.last_strategies = None
 
         infosets = set()
+
         def _traverse(state, d):
             if state.is_terminal() or d >= self.depth:
                 return
@@ -16,14 +28,28 @@ class PlotStrategyPCA:
                 infosets.add((state.player, state.observations[state.player], state.actions))
             for ai, a in enumerate(state.actions):
                 _traverse(self.game.play(state, index=ai), d + 1)
-        _traverse(self.game.start(), 0)
-        self.infosets = sorted(list(infosets))
 
+        _traverse(self.game.start(), 0)
+
+        # List of infosets measured: [(player, observation, actions)]
+        self.infosets = sorted(list(infosets))
+        # Iterations, shape (t, )
         self.d_t = []
+        # Regrets, shape (t, players)
+        self.d_exploitability = []
+        # Strategy policies in measured infosets, shape (t, actions)
+        # `actions` are: first infoset actions, second infoset actions, ...
         self.d_strat = []
+        # Regret matching policies in measured infosets, shape (t, actions)
+        # Where regret is undefined uses same policy as strategy
         self.d_regret = []
 
-    def append(self, t, strategies):
+    def append(self, t, strategies, exploitabilities=None):
+        """
+        Record strategy profile observation at time `t` (iterations).
+
+        Optionally also records the given exploitabilities.
+        """
         assert len(strategies) == self.game.players
         assert (not self.d_t) or self.d_t[-1] < t
 
@@ -42,94 +68,206 @@ class PlotStrategyPCA:
         self.d_t.append(t)
         self.d_strat.append(np.array(strat_vec))
         self.d_regret.append(np.array(reg_vec))
+        if exploitabilities is None:
+            exploitabilities = [np.nan, np.nan]
+        assert len(exploitabilities) == self.game.players
+        self.d_exploitability.append(exploitabilities)
+        self.last_strategies = strategies
 
     def infoset_weight_vec(self, strategies):
+        """
+        Compute the probability of every tracked infoset being visited (within given depth).
+
+        Returns array of shape `(actions, )`.
+        """
         assert len(strategies) == self.game.players
         strat_ps = {i: 0.0 for i in self.infosets}
-        #reg_ps = {i: 0.0 for i in self.infosets}
 
-        def _traverse(state, strat_p, reg_p, d):
+        def _traverse(state, strat_p, d):
             iset = state.observations[state.player]
             idx = (state.player, iset, state.actions)
             if state.is_terminal() or d >= self.depth:
                 return
             if not state.is_chance():
                 strat_ps[idx] += strat_p
-                #reg_ps[idx] += reg_p
                 s = strategies[state.player]
                 strat = s.strategy(state)
-                reg = strat
-                if isinstance(s, RegretStrategy):
-                    entry = s.get_entry(iset, len(state.actions))
-                    reg = s.regret_matching(entry[0])
             else:
                 strat = state.chance
-                reg = strat
             for ai, a in enumerate(state.actions):
-                _traverse(self.game.play(state, index=ai), strat_p * strat[ai], reg_p * reg[ai], d + 1)
+                _traverse(self.game.play(state, index=ai), strat_p * strat[ai], d + 1)
 
-        _traverse(self.game.start(), 1.0, 1.0, 0)
+        _traverse(self.game.start(), 1.0, 0)
         strat_vec = []
-        #reg_vec = []
         for idx in self.infosets:
             strat_vec.extend([strat_ps[idx]] * len(idx[2]))
-            #reg_vec.extend([reg_ps[idx]] * len(idx[2]))
-        return np.array(strat_vec)  #, np.array(reg_vec))
+        return np.array(strat_vec)
 
-    def plot(self, base=None, color=None, smooth_regret=0, with_regret=False):
+    def plot_trace(self, base, color, smooth_regret=5, with_regret=True):
+        """
+        Create Plotly objects to plot the trace.
+
+        Base must have shape `(2, tot_actions)`.
+        Returns Plotly graph objects. Optionally also returns regret traces.
+        """
         import plotly.graph_objs as pgo
 
         d_t = np.array(self.d_t)
         d_text = ["{} its".format(t) for t in d_t]
+        d_text_expl = []
+        for dti, dt in enumerate(d_text):
+            exp = self.d_exploitability[dti]
+            if np.isfinite(exp).any():
+                exptext = ", ".join("{:.3f}".format(v) for v in exp)
+                d_text_expl.append("{} expls {}".format(dt, exptext))
         d_strat = np.array(self.d_strat)
         d_regret = np.array(self.d_regret)
-        if base is None:
-            base = self.common_base([self], with_regret=with_regret)
         assert base.shape == (2, d_strat.shape[1])
-        
+
         proj_strat = np.matmul(d_strat, base.transpose())
         objs = [
-            pgo.Scatter(x=proj_strat[:, 0], y=proj_strat[:, 1], name=self.name + " strategy", 
-                        mode="lines+markers", hovertext=d_text, hoverinfo="text+name", 
-                        line=dict(color=color, width=1.8, shape='spline'),
-                        marker=dict(symbol="x", color=color, size=[0] * (proj_strat.shape[0] - 1) + [10])),
+            pgo.Scatter(
+                x=proj_strat[:, 0],
+                y=proj_strat[:, 1],
+                name=self.name + " strategy",
+                mode="lines+markers",
+                legendgroup=str(id(self)),
+                hovertext=d_text,
+                hoverinfo="text+name",
+                hoverlabel=dict(namelength=-1),
+                line=dict(color=color, width=1.8, shape='spline'),
+                marker=dict(
+                    symbol="x",
+                    line=dict(width=0.2, color='black'),
+                    color=color,
+                    size=[0] * (proj_strat.shape[0] - 1) + [10])),
         ]
         if with_regret:
             proj_regret = np.matmul(d_regret, base.transpose())
             if smooth_regret > 1:
                 from scipy import signal
-                proj_regret = signal.savgol_filter(proj_regret, smooth_regret, min(4, smooth_regret - 1), axis=0)
+                proj_regret = signal.savgol_filter(
+                    proj_regret, smooth_regret, min(4, smooth_regret - 1), axis=0)
             objs.extend([
-                pgo.Scatter(x=proj_regret[:, 0], y=proj_regret[:, 1], name=self.name + " regret", 
-                            mode="lines+markers", hovertext=d_text, hoverinfo="text+name", 
-                            line=dict(color=color, width=0.7, shape='spline', smoothing=0.3),
-                            marker=dict(symbol="x", color=color, opacity=0.7, size=[0] * (proj_regret.shape[0] - 1) + [10])),
+                pgo.Scatter(
+                    x=proj_regret[:, 0],
+                    y=proj_regret[:, 1],
+                    name=self.name + " regret",
+                    mode="lines+markers",
+                    hovertext=d_text,
+                    hoverinfo="text+name",
+                    hoverlabel=dict(namelength=-1),
+                    visible='legendonly',
+                    line=dict(color=color, width=0.7, shape='spline', smoothing=0.3),
+                    marker=dict(
+                        line=dict(width=0.2, color='black'),
+                        symbol="x",
+                        color=color,
+                        opacity=0.7,
+                        size=[0] * (proj_regret.shape[0] - 1) + [10])),
             ])
         return objs
 
+    def plot_exploitabilities(self, color, showlegend=True):
+        """
+        Create Plotly objects to plot the exploitability.
+
+        Base must have shape `(2, tot_actions)`.
+        Returns Plotly graph objects. Optionally also returns regret traces.
+        """
+        import plotly.graph_objs as pgo
+
+        d_t = np.array(self.d_t)
+        d_exp = np.array(self.d_exploitability)
+
+        objs = []
+        for p in range(self.game.players):
+            objs.append(
+                pgo.Scatter(
+                    x=d_t,
+                    y=d_exp[:, p],
+                    name="{} player {} exploitability".format(self.name, p),
+                    mode="lines",
+                    legendgroup=str(id(self)),
+                    hoverinfo="x+y+name",
+                    hoverlabel=dict(namelength=-1),
+                    showlegend=showlegend,
+                    line=dict(color=color, width=1.5)))
+        return objs
+
     @classmethod
-    def common_base(cls, traces, with_regret=False, weight_vec=None):
+    def common_PCA_base(cls, traces, with_regret=True, weight_vec=None, dims=2, seed=None):
+        """
+        Compute a commpon PCA base of the given trace set.
+        
+        Returns unit L2-norm base of shape `(dims, tot_actions)`.
+        """
         import sklearn
         from sklearn.decomposition import PCA
 
         rows = [t.d_strat for t in traces]
         if with_regret:
             rows.extend([t.d_regret for t in traces])
-        pca = PCA(2)
+        pca = PCA(dims, random_state=seed)
         rows = np.concatenate(rows, axis=0)
         if weight_vec is not None:
-            rows *= [weight_vec] 
+            rows *= [weight_vec]
         pca.fit(rows)
-        return pca.components_
+        base = pca.components_
+        assert base.shape[0] == dims
+        base = base / sp.linalg.norm(base, axis=1).reshape((-1, 1))
+        return base
 
     @classmethod
-    def common_plot(cls, traces, base=None, palette=None, with_regret=False):
-        if base is None:
-            base = cls.common_base(traces, with_regret=with_regret)
+    def common_random_base(cls, traces, weight_vec=None, dims=2, seed=None):
+        """
+        Compute a random base of the given trace set.
+
+        Returns unit L2-norm base of shape `(dims, tot_actions)`.
+        Optionally weights the actions by weight_vec
+        """
+        base = np.random.normal(size=(dims, len(traces[0].d_strat[0])))
+        if weight_vec is not None:
+            base *= [weight_vec]
+        base = base / sp.linalg.norm(base, axis=1).reshape((-1, 1))
+        assert base.shape[0] == dims
+        return base
+
+    @classmethod
+    def common_plot(cls,
+                    traces,
+                    base,
+                    plot_traces=True,
+                    plot_exploitabilities=True,
+                    palette=None,
+                    with_regret=True,
+                    title=None):
+        """
+        Create a Plotly plot with trace and exploitability subplots.
+        """
+        import plotly
+
+        assert plot_exploitabilities or plot_traces
+        if plot_traces and plot_exploitabilities:
+            #fig = plotly.tools.make_subplots(insets=[dict(cell=(1,1), l=0.6, w=0.4, h=0.4, b=0.05)], print_grid=False)
+            fig = plotly.tools.make_subplots(cols=2, print_grid=False, column_width=[0.6, 0.4])
+        else:
+            fig = plotly.graph_objs.Figure()
+        if plot_traces:
+            fig['layout']['yaxis']['scaleanchor'] = 'x'
+        #if plot_exploitabilities:
+        #    fig['layout']['yaxis2' if plot_traces else 'yaxis']['type'] = 'log'
         if palette is None:
-            import bokeh.palettes
-            palette = bokeh.palettes.Category20_20
-        objs = []
+            palette = plotly.colors.DEFAULT_PLOTLY_COLORS
         for ti, t in enumerate(traces):
-            objs.extend(t.plot(base=base, color=palette[ti], with_regret=with_regret))
-        return objs
+            if plot_traces:
+                fig.add_traces(t.plot_trace(base=base, color=palette[ti], with_regret=with_regret))
+            if plot_exploitabilities:
+                objs = t.plot_exploitabilities(color=palette[ti], showlegend=not plot_traces)
+                if plot_traces:
+                    for o in objs:
+                        o.xaxis = 'x2'
+                        o.yaxis = 'y2'
+                fig.add_traces(objs)
+        fig['layout'].update(hovermode='x', title=dict(text=title))
+        return fig
