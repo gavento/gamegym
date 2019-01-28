0924c94c74bc46f9fa1251866a9827d2e2749091
Tomas Gavenciak <gavento@ucw.cz>
2019-01-28 12:04:25 +0100

    Fix included packages in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gavento/gamegym",
-    packages=setuptools.find_packages(),#include=['gamegym', 'gamegym.*']),
+    packages=setuptools.find_packages(include=['gamegym', 'gamegym.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
